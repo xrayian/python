f8fdfdc5ae59efb444cd85842ac7ed5ba539bb9c
Rayian Mahi <rbsmahi@gmail.com>
2021-11-13 03:33:50 +0600

    reformatted and overall prettified the code

--- 3n+1.py
+++ 3n+1.py
@@ -1,7 +1,8 @@
 from datetime import datetime
 
-incarnations:int = 0
-highest:int = 0
+attempt_count: int = 0
+highest: int = 0
+seed_value: int = 0
 starting_time = datetime.now()
 
 
@@ -9,42 +10,53 @@ def getDuration():
     global starting_time
     return (datetime.now() - starting_time)
 
+
 def resetTimer():
     global starting_time
     starting_time = datetime.now()
 
 
-def conjecture(num:int):
+def conjecture(num: int):
 
-    global incarnations
+    global attempt_count
     global highest
-    
-    incarnations = incarnations + 1
-    
+    global seed_value
+
+    attempt_count = attempt_count + 1
+
     if (num > highest):
-            highest = num
-    
-    
-    if (num % 2 != 0): #if Odd
+        highest = num
+
+    if (num % 2 != 0):  # if Odd
         print(int(num))
-        new_num:int = (3 * num) + 1
-    else :
+        new_num: int = (3 * num) + 1
+    else:
         print(int(num))
-        new_num:int = (num // 2)
-    
+        new_num: int = (num // 2)
+
     if (num != 1.0):
         # if (new_num > highest):
         #     highest = new_num
         return new_num
     else:
-        print(f"\n-----\n\nCollatz conjecture encountered after {incarnations - 3 if incarnations > 3 else 0} attempts!\nExecution time: {getDuration()}s\nHighest: {highest}\n")
-        incarnations = 0
+        print(f"""
+-----
+
+Collatz conjecture encountered after {attempt_count - 3 if attempt_count > 3 else 0} attempts!
+Execution time: {getDuration()}
+Seed: {seed_value}
+Highest: {highest}
+Increased: {highest - seed_value}
+
+        """)
+        attempt_count = 0
         highest = 0
+        seed_value = 0
+
         return "halt"
 
 
-
-def main_loop(output_value): #initial input is also called output_value for lack of coffee
+def main_loop(output_value):  # initial input is also called output_value for lack of coffee
     resetTimer()
     print("-----\n")
     while True:
@@ -52,6 +64,7 @@ def main_loop(output_value): #initial input is also called output_value for lack
             break
         output_value = conjecture(output_value)
 
+
 while True:
-    main_loop(int(input("Enter a number [except 1]: ")))
-    
+    seed_value = int(input("Enter a number [except 1]: "))
+    main_loop(seed_value)
